48a3de5e343a351754aa4b79a00a56116d717ff8
Dan Kral <dankral01@gmail.com>
2022-04-03 19:37:39 -0400

    initial commit; neat animation! worthless

--- App.js
+++ App.js
@@ -1,26 +1,85 @@
-import React, { useEffect } from 'react';
-import { SafeAreaView, StyleSheet, View, Image } from 'react-native';
-import { Gyroscope } from 'expo-sensors';
-import MockUI from './MockUI';
+import React, { useRef } from 'react';
+import { Text, SafeAreaView, StyleSheet, View, PanResponder, Image, Dimensions } from 'react-native';
+// import { Gyroscope } from 'expo-sensors';
 import Animated, {
-  Easing,
-  interpolate,
+  withTiming,
   useAnimatedStyle,
   useDerivedValue,
   useSharedValue,
-  withSpring,
+  Easing
 } from 'react-native-reanimated';
+import { LinearGradient } from 'expo-linear-gradient';
+import { Platform } from 'react-native-web';
 
 const App = () => {
-  const gyroValue = useSharedValue({ x: 0, y: 0, z: 0 });
+  const gyroValue = useSharedValue({ x: 0, y: 0 });
   const prev = useSharedValue({ x: 0, y: 0 });
+  const { width, height } = Dimensions.get('window');
+
+  function convertAbsToRelative({ x, y }) {
+
+    return {
+      x: x - (width / 2),
+      y: y - (height / 2)
+    }
+  }
+  const panResponder = useRef(
+    PanResponder.create({
+      // Ask to be the responder:
+      onStartShouldSetPanResponder: (evt, gestureState) => true,
+      onStartShouldSetPanResponderCapture: (evt, gestureState) =>
+        true,
+      onMoveShouldSetPanResponder: (evt, gestureState) => true,
+      onMoveShouldSetPanResponderCapture: (evt, gestureState) =>
+        true,
+
+      onPanResponderGrant: (evt, gestureState) => {
+        // The gesture has started. Show visual feedback so the user knows
+        // what is happening!
+        // gestureState.d{x,y} will be set to zero now
+        // console.log("touch", gestureState)
+        gyroValue.value = convertAbsToRelative({
+          x: gestureState.x0,
+          y: gestureState.y0
+        })
+
+
+      },
+      onPanResponderMove: (evt, gestureState) => {
+        // The most recent move distance is gestureState.move{X,Y}
+        // The accumulated gesture distance since becoming responder is
+        // console.log("move ", gestureState)
+        gyroValue.value = convertAbsToRelative({
+          x: gestureState.moveX,
+          y: gestureState.moveY
+        })
+
+      },
+      onPanResponderTerminationRequest: (evt, gestureState) =>
+        true,
+      onPanResponderRelease: (evt, gestureState) => {
+        // The user has released all touches while this view is the
+        // responder. This typically means a gesture has succeeded
+
+        gyroValue.value = { x: 0, y: 0 };
+      },
+      onPanResponderTerminate: (evt, gestureState) => {
+        // Another component has become the responder, so this gesture
+        // should be cancelled
+        gyroValue.value = { x: 0, y: 0 };
+
+      },
+    })
+  ).current;
+
   const derivedTranslations = useDerivedValue(() => {
     'worklet';
-    const MAX_X = 40;
-    const MAX_Y = 40;
 
-    let newX = prev.value.x + gyroValue.value.y * -2;
-    let newY = prev.value.y + gyroValue.value.x * -2;
+    const MAX_X = width / 2;
+    const MAX_Y = height / 2;
+
+    let newX = gyroValue.value.x;
+    let newY = gyroValue.value.y;
 
     // Can be more cleaner
     if (Math.abs(newX) >= MAX_X) {
@@ -29,6 +88,7 @@ const App = () => {
     if (Math.abs(newY) >= MAX_Y) {
       newY = prev.value.y;
     }
+
     prev.value = {
       x: newX,
       y: newY,
@@ -39,43 +99,20 @@ const App = () => {
     };
   }, [gyroValue.value]);
 
-  useEffect(() => {
-    Gyroscope.addListener(({ x, y, z }) => {
-      gyroValue.value = { x, y, z };
-    })
-    // const subscription = gyroscope.subscribe(({ x, y, z, timestamp }) => {
-    // });
-    return () => {
-      Gyroscope.removeAllListeners()
-    };
-  }, [gyroValue.value]);
-
-  const AnimatedStyles = {
+  const animatedStyles = {
     motion: useAnimatedStyle(() => {
-      const inputRange = [-100, 0, 100];
 
-      const outputRange = [-20, 0, 20];
       return {
         transform: [
           {
-            translateX: withSpring(
-              interpolate(
-                derivedTranslations.value.x,
-                inputRange,
-                outputRange,
-                // Easing.bezier(0.16, 1, 0.3, 1),
-              ),
-            ),
+            perspective: 1000
           },
           {
-            translateY: withSpring(
-              interpolate(
-                derivedTranslations.value.y,
-                inputRange,
-                outputRange,
-                // Easing.bezier(0.16, 1, 0.3, 1),
-              ),
-            ),
+            rotateY:
+              derivedTranslations.value.x / (width / 2) * 13 + "deg",
+          },
+          {
+            rotateX: -derivedTranslations.value.y / (height / 2) * 15 + "deg",
           },
         ],
       };
@@ -85,14 +122,29 @@ const App = () => {
   return (
     <SafeAreaView>
       <View style={styles.cont}>
-        <Animated.Image
-          source={require('./assets/splash.png')}
-          style={[styles.background]}
-          blurRadius={2}
-          resizeMode="cover"
-        />
-        <Animated.View style={AnimatedStyles.motion}>
-          <View></View>
+        <Animated.View
+          style={[styles.background,
+            //  animatedStyles.motion
+          ]}
+
+        >
+          <LinearGradient
+            // Button Linear Gradient
+            colors={['#00b3ff', '#ffa939']}
+            style={styles.fill}
+            end={{ x: 1, y: 0.5 }}
+          >
+            <Animated.View
+              style={[styles.card, animatedStyles.motion]}
+              {...panResponder.panHandlers}
+            >
+              {/* <Text style={{ color: "#FFF" }}>wow.</Text> */}
+              <View />
+              <View style={{ backgroundColor: "#222", borderRadius: 6, width: 50, height: 40 }} />
+              <View style={{ backgroundColor: "#222", borderRadius: 6, width: "80%", height: 25 }} />
+
+            </Animated.View>
+          </LinearGradient>
         </Animated.View>
       </View>
     </SafeAreaView>
@@ -101,21 +153,41 @@ const App = () => {
 
 const styles = StyleSheet.create({
   cont: {
-    height: '100%',
-    width: '100%',
+    height: Dimensions.get("window").height,
+    width: Dimensions.get("window").width,
     backgroundColor: '#111',
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
   },
   background: {
-    position: 'absolute',
+    // position: 'absolute',
     alignItems: 'center',
-    aspectRatio: 1,
-    bottom: '-5%',
-    width: '120%',
-    height: '120%',
+    justifyContent: 'center',
+    // zIndex: 10,
+    backgroundColor: "#0090ff",
+    // aspectRatio: 1,
+    // bottom: '-5%',
+    width: "80%",
+    height: 200,
+    borderRadius: 12,
   },
+  card: {
+    width: "99.5%",
+    height: "99.5%",
+    backgroundColor: "#111",
+    borderRadius: 12,
+    // alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 30
+  },
+  fill: {
+    width: "100%",
+    height: "100%",
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 12
+  }
 });
 
 export default App;
